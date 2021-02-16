0c599f50c698a6dc9b380d3df6b80c7d0039d444
Maxim <57855089+m4sk3k@users.noreply.github.com>
2021-02-16 12:06:07 +0900

    Lab13

---
+++ TooSimpleFrame.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+public class TooSimpleFrame extends Frame{
+    public static void main(String[] args) {
+        Frame fr = new TooSimpleFrame();
+        fr.setSize(400,400);
+        fr.setVisible(true);
+    }
+}
